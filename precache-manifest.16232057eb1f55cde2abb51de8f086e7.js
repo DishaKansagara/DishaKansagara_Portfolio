73f588bc360fa1d3b93e72ef39268573d43f31ba
DishaKansagara <dishakansagara9@gmail.com>
2021-05-22 11:45:24 -0400

    Updates

---
+++ precache-manifest.16232057eb1f55cde2abb51de8f086e7.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c1952df2b9745d39ec68bf8e00c006",
+    "url": "/DishaKansagara_Portfolio/index.html"
+  },
+  {
+    "revision": "6eed13f340b019791ede",
+    "url": "/DishaKansagara_Portfolio/static/css/main.6bdfadc8.chunk.css"
+  },
+  {
+    "revision": "2310264926e7a53ba894",
+    "url": "/DishaKansagara_Portfolio/static/js/2.1f8bde27.chunk.js"
+  },
+  {
+    "revision": "c9e6461fc31e6a3d2ff16ff3d3ef45a7",
+    "url": "/DishaKansagara_Portfolio/static/js/2.1f8bde27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eed13f340b019791ede",
+    "url": "/DishaKansagara_Portfolio/static/js/main.08f9933f.chunk.js"
+  },
+  {
+    "revision": "e6e64e2b98c942cf0442",
+    "url": "/DishaKansagara_Portfolio/static/js/runtime-main.7ad1cb01.js"
+  },
+  {
+    "revision": "3bb76b7234f7f37b9b93ffc842726769",
+    "url": "/DishaKansagara_Portfolio/static/media/MATRIX.3bb76b72.jpg"
+  },
+  {
+    "revision": "62d356ed205f2ecd56492ee17b0d5e0c",
+    "url": "/DishaKansagara_Portfolio/static/media/MovieStore.62d356ed.png"
+  },
+  {
+    "revision": "487206c1d6e105e8fc3f33e9992e3a59",
+    "url": "/DishaKansagara_Portfolio/static/media/Pic.487206c1.jpeg"
+  },
+  {
+    "revision": "3a36a98aa8b5361089d769ff541a2e0f",
+    "url": "/DishaKansagara_Portfolio/static/media/androidapp.3a36a98a.png"
+  },
+  {
+    "revision": "819e14abfb28d33cf6f1293eed6342c6",
+    "url": "/DishaKansagara_Portfolio/static/media/angular.819e14ab.png"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/DishaKansagara_Portfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/DishaKansagara_Portfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/DishaKansagara_Portfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/DishaKansagara_Portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/DishaKansagara_Portfolio/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/DishaKansagara_Portfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "5874f897c58d6ad9d4ae125687606c3b",
+    "url": "/DishaKansagara_Portfolio/static/media/findpenguins.5874f897.png"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/DishaKansagara_Portfolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "088bfa6005dc4fca0b96cdbcc5c83b9a",
+    "url": "/DishaKansagara_Portfolio/static/media/heroku1.088bfa60.png"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/DishaKansagara_Portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "f039e61a203f7e234bc7d6b83c2d86e8",
+    "url": "/DishaKansagara_Portfolio/static/media/java.f039e61a.jpg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/DishaKansagara_Portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/DishaKansagara_Portfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/DishaKansagara_Portfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/DishaKansagara_Portfolio/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/DishaKansagara_Portfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/DishaKansagara_Portfolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/DishaKansagara_Portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/DishaKansagara_Portfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/DishaKansagara_Portfolio/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/DishaKansagara_Portfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/DishaKansagara_Portfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/DishaKansagara_Portfolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "3ed8a8cadb6996e1f631e2ef4b99a7d7",
+    "url": "/DishaKansagara_Portfolio/static/media/travel.3ed8a8ca.png"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/DishaKansagara_Portfolio/static/media/typescript.3de182d2.svg"
+  }
+]);
